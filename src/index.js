ab4e8be839010ee06bbd5c45e5a5da387f436ddc
shamedzka <shamedzka@gmail.com>
2021-08-21 18:24:23 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let positiveNumber = Math.abs(n);
+   let stringNumber = positiveNumber.toString();
+   let arr = stringNumber.split('').reverse().join('');
+   return Number(arr);
 }
